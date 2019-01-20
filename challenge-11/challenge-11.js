e195f600f0ad3a2e379f623c80290fdf8af4f0b5
Rui Alves <scpalves@gmail.com>
2019-01-20 19:48:17 +0000

    Update challenge-11.js

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -7,14 +7,20 @@ loop, mostre no console a mensagem:
 'Entrou ao menos uma vez!'
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
-// ?
+while (once = true){
+    
+    console.log('Entrou ao menos uma vez!')
+    break  
+}
 
 /*
 Crie um objeto chamado `person`, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
-// ?
+var person = {name:'Rui',age:40,weight:92,birthday:'09/07/1977'}
+
+
 
 /*
 Use um loop para percorrer o objeto criado acima, mostrando no console
@@ -25,7 +31,22 @@ esse objeto tem.
 Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
-// ?
+for (i in person){    
+    console.log('The '+ i +' of person is '+ person[i]) 
+}
+
+var counter = 0;
+
+for (i in person){    
+ console.log('The '+ i +' of person is '+ person[i])
+counter ++;
+     }
+
+console.log('The person has ' + counter + ' properties') 
+ The person has 3 properties
+
+
+
 
 /*
 Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
@@ -34,7 +55,14 @@ Se verdadeiro, retornar `true`. Senão, retornar false.
 Após a função, mostrar a mensagem no console:
 'The person has more than 25 years old? [TRUE/FALSE]'
 */
-// ?
+function moreThan(idade){
+  if (person.age > idade ){   
+   return true;
+  }  
+    return false; 
+}
+console.log('The person has more than 25 years old?'+ moreThan(25))
+
 
 /*
 Faça um loop de 0 a 20, que adicione cada número como um item de um
@@ -42,7 +70,16 @@ array chamado `numbers`. Se o contador for maior que 10, saia do loop.
 Mostre no console os números no array.
 */
 console.log( 'De 0 a 10:' );
-// ?
+var numbers = [];
+for (i = 0  ; i < 20 ; i++){  
+     numbers.push(i);
+    if (numbers.length > 10){
+   
+    break;
+    } 
+}
+console.log(numbers)
+
 
 /*
 Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
